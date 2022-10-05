cb9d32f17b395fa2e6d0d2b8113912d3d405b007
Mike Castner <mjcastner@gmail.com>
2022-10-05 18:20:38 -0400

    Iteration on page logic

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,146 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
       ),
-      home: DevRootPage(),
+      home: DevRoot(),
     );
   }
 }
 
-class DevRootPage extends StatelessWidget {
+class DevRoot extends StatefulWidget {
+  @override
+  _DevRootState createState() => _DevRootState();
+}
+
+class _DevRootState extends State<DevRoot> {
+  bool _isFinished = false;
+  final List<Widget> _resultsWidgets = [
+    const SizedBox(height: 10),
+    Text(
+      'FinishedProjects/',
+      style: GoogleFonts.robotoMono(color: Colors.green),
+    ),
+    InkWell(
+      onTap: () {
+        launchUrl(Uri.parse('https://github.com/mjcastner/edsm_bq'));
+      },
+      child: Text(
+        '    edsm_bq',
+        style: GoogleFonts.robotoMono(color: Colors.green),
+      ),
+    ),
+    InkWell(
+      onTap: () {
+        launchUrl(Uri.parse(
+            'https://colab.research.google.com/drive/1VH3m-7zuCURVVf4p1ExRCd7rNs4AFeDG?usp=sharing'));
+      },
+      child: Text(
+        '    edsm_bq_colab',
+        style: GoogleFonts.robotoMono(color: Colors.green),
+      ),
+    ),
+    InkWell(
+      onTap: () {
+        launchUrl(Uri.parse('https://github.com/mjcastner/jira_to_github'));
+      },
+      child: Text(
+        '    jira_to_github',
+        style: GoogleFonts.robotoMono(color: Colors.green),
+      ),
+    ),
+    InkWell(
+      onTap: () {
+        launchUrl(Uri.parse('https://github.com/kiera-dev/tarot'));
+      },
+      child: Text(
+        '    tarot_flutter',
+        style: GoogleFonts.robotoMono(color: Colors.green),
+      ),
+    ),
+    const SizedBox(height: 5),
+    Text(
+      'InProgressProjects/',
+      style: GoogleFonts.robotoMono(color: Colors.green),
+    ),
+    InkWell(
+      onTap: () {
+        launchUrl(Uri.parse('https://github.com/mjcastner/go-home'));
+      },
+      child: Text(
+        '    go-home',
+        style: GoogleFonts.robotoMono(color: Colors.green),
+      ),
+    ),
+    InkWell(
+      onTap: () {
+        launchUrl(Uri.parse('https://github.com/mjcastner/zunify'));
+      },
+      child: Text(
+        '    zunify',
+        style: GoogleFonts.robotoMono(color: Colors.green),
+      ),
+    ),
+    const SizedBox(height: 5),
+    Text(
+      'NeatStuffIveWorkedOn/',
+      style: GoogleFonts.robotoMono(color: Colors.green),
+    ),
+    InkWell(
+      onTap: () {
+        launchUrl(Uri.parse('https://android.com'));
+      },
+      child: Text(
+        '    android',
+        style: GoogleFonts.robotoMono(color: Colors.green),
+      ),
+    ),
+    InkWell(
+      onTap: () {
+        launchUrl(Uri.parse('https://fuchsia.dev'));
+      },
+      child: Text(
+        '    fuchsia',
+        style: GoogleFonts.robotoMono(color: Colors.green),
+      ),
+    ),
+    InkWell(
+      onTap: () {
+        launchUrl(Uri.parse('https://github.com/apps/github-learning-lab'));
+      },
+      child: Text(
+        '    learning_lab',
+        style: GoogleFonts.robotoMono(color: Colors.green),
+      ),
+    ),
+    InkWell(
+      onTap: () {
+        launchUrl(Uri.parse('https://safebrowsing.google.com/'));
+      },
+      child: Text(
+        '    safe_browsing',
+        style: GoogleFonts.robotoMono(color: Colors.green),
+      ),
+    ),
+    InkWell(
+      onTap: () {
+        launchUrl(Uri.parse('https://cloud.google.com/spanner/'));
+      },
+      child: Text(
+        '    spanner',
+        style: GoogleFonts.robotoMono(color: Colors.green),
+      ),
+    ),
+    const SizedBox(height: 5),
+    Text(
+      'resume.pdf',
+      style: GoogleFonts.robotoMono(color: Colors.green),
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,138 +178,25 @@ class DevRootPage extends StatelessWidget {
                   ),
                 ),
                 AnimatedTextKit(
+                  onFinished: () {
+                    setState(() {
+                      _isFinished = true;
+                    });
+                  },
                   isRepeatingAnimation: false,
                   animatedTexts: [
                     TypewriterAnimatedText(
                       'ls -1',
-                      speed: const Duration(milliseconds: 300),
+                      speed: const Duration(milliseconds: 100),
                       textStyle: GoogleFonts.robotoMono(color: Colors.green),
                     ),
                   ],
                 ),
               ],
             ),
-            const SizedBox(height: 10),
-            Text(
-              'FinishedProjects/',
-              style: GoogleFonts.robotoMono(color: Colors.green),
-            ),
-            InkWell(
-              onTap: () {
-                launchUrl(Uri.parse('https://github.com/mjcastner/edsm_bq'));
-              },
-              child: Text(
-                '    edsm_bq',
-                style: GoogleFonts.robotoMono(color: Colors.green),
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                launchUrl(Uri.parse(
-                    'https://colab.research.google.com/drive/1VH3m-7zuCURVVf4p1ExRCd7rNs4AFeDG?usp=sharing'));
-              },
-              child: Text(
-                '    edsm_bq_colab',
-                style: GoogleFonts.robotoMono(color: Colors.green),
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                launchUrl(
-                    Uri.parse('https://github.com/mjcastner/jira_to_github'));
-              },
-              child: Text(
-                '    jira_to_github',
-                style: GoogleFonts.robotoMono(color: Colors.green),
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                launchUrl(Uri.parse('https://github.com/kiera-dev/tarot'));
-              },
-              child: Text(
-                '    tarot_flutter',
-                style: GoogleFonts.robotoMono(color: Colors.green),
-              ),
-            ),
-            const SizedBox(height: 5),
-            Text(
-              'InProgressProjects/',
-              style: GoogleFonts.robotoMono(color: Colors.green),
-            ),
-            InkWell(
-              onTap: () {
-                launchUrl(Uri.parse('https://github.com/mjcastner/go-home'));
-              },
-              child: Text(
-                '    go-home',
-                style: GoogleFonts.robotoMono(color: Colors.green),
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                launchUrl(Uri.parse('https://github.com/mjcastner/zunify'));
-              },
-              child: Text(
-                '    zunify',
-                style: GoogleFonts.robotoMono(color: Colors.green),
-              ),
-            ),
-            const SizedBox(height: 5),
-            Text(
-              'NeatStuffIveWorkedOn/',
-              style: GoogleFonts.robotoMono(color: Colors.green),
-            ),
-            InkWell(
-              onTap: () {
-                launchUrl(Uri.parse('https://android.com'));
-              },
-              child: Text(
-                '    android',
-                style: GoogleFonts.robotoMono(color: Colors.green),
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                launchUrl(Uri.parse('https://fuchsia.dev'));
-              },
-              child: Text(
-                '    fuchsia',
-                style: GoogleFonts.robotoMono(color: Colors.green),
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                launchUrl(
-                    Uri.parse('https://github.com/apps/github-learning-lab'));
-              },
-              child: Text(
-                '    learning_lab',
-                style: GoogleFonts.robotoMono(color: Colors.green),
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                launchUrl(Uri.parse('https://safebrowsing.google.com/'));
-              },
-              child: Text(
-                '    safe_browsing',
-                style: GoogleFonts.robotoMono(color: Colors.green),
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                launchUrl(Uri.parse('https://cloud.google.com/spanner/'));
-              },
-              child: Text(
-                '    spanner',
-                style: GoogleFonts.robotoMono(color: Colors.green),
-              ),
-            ),
-            const SizedBox(height: 5),
-            Text(
-              'resume.pdf',
-              style: GoogleFonts.robotoMono(color: Colors.green),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: _isFinished ? _resultsWidgets : [],
             ),
           ],
         ),
